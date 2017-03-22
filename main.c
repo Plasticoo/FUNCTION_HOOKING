84b9bb0118da3bf9e0bc125fd58b5a717bb401c6
Romeu Gomes <oh4ianon@hotmail.com>
2017-03-22 17:59:37 +0000

    Added body of the main function.

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 long long multiply(int a, int b)
 {
@@ -11,4 +12,11 @@ long long multiply(int a, int b)
 
 int main()
 {
+    int a = 2;
+    int b = 1;
+
+    for(;;a++,b++) {
+        printf("Multiply() normal: %lld\n", multiply(a, b));
+        sleep(1);
+    }
 }
