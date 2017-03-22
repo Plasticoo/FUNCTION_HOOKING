d23a661624490b8ff3669fd6ae3ea31f3d781ae1
Romeu Gomes <oh4ianon@hotmail.com>
2017-03-22 18:18:30 +0000

    Finish shared object.

--- libhook.c
+++ libhook.c
@@ -5,13 +5,19 @@
 #include <stdio.h>
 
 unsigned int (*original_sleep)(unsigned int);
+long long (*multiply)(int,int);
+
+int a = 300;
+int b = 400;
 
 void __attribute__((constructor)) __init()
 {
     original_sleep = dlsym(RTLD_NEXT, "sleep");
+    multiply = 0x400546;
 }
 
 unsigned int sleep(unsigned int seconds)
 {
-    
+    printf("Multiply() hooked: %lld\n", multiply(a++, b++));
+    return (*original_sleep)(seconds);
 }
